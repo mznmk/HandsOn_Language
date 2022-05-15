728c7dd4d95e497ec9f0b53dc925ba5655ddb885
mznmk <mika_develop@mznmk.com>
2022-05-15 23:02:37 +0900

    [TGU Node] finish "sec8-45"

---
+++ TGU_Intro_to_Node/sec08/45/clock.js
@@ -0,0 +1,26 @@
+var EventEmitter = require('events');
+
+var Clock = class extends EventEmitter {
+  constructor() {
+    super();
+    this.interval = 2000;
+    this.timer = null;
+  }
+  start() {
+    if (this.timer) {
+      this.stop();
+    }
+    this.timer = global.setInterval(() => {
+      this.emit("tick");
+    }, this.interval);
+  }
+  stop() {
+    if (!this.timer) {
+      return;
+    }
+    global.clearInterval(this.timer);
+    this.timer = false;
+  }
+};
+
+module.exports = Clock;
