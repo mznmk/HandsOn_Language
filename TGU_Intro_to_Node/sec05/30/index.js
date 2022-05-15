ea488d9d01979058c3ba2b78eab561344a80306d
mznmk <mika_develop@mznmk.com>
2022-05-15 18:12:12 +0900

    [TGU Node] finish "sec5-30"

---
+++ TGU_Intro_to_Node/sec05/30/index.js
@@ -0,0 +1,24 @@
+var Car = class {
+	constructor(name) {
+		this.name = name;
+	}
+	drive() {
+		console.log("zoom zoom ...");
+	}
+};
+
+var Lamborghini = class extends Car {
+  constructor(name) {
+    super(name);
+  }
+  echo() {
+    super.drive();
+  }
+  drive() {
+    console.log(`fire ${this.name}`);
+  }
+}
+
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive()
