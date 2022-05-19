0890069bad3a1269bcfa14c52b307a38b5ae0bbd
mznmk <mika_develop@mznmk.com>
2022-05-19 15:54:54 +0900

    [MS TS] finish sec02-011

---
+++ MS_Understanding_TS_2020/sec02/011/app.js
@@ -0,0 +1,12 @@
+function add(n1, n2) {
+    // if (typeof(n1) !== 'number' || typeof(n2) !== 'number') {
+    //   throw new Error('入力値は正しくありません');
+    // }
+    return n1 + n2;
+}
+{
+    var number1 = 42;
+    var number2 = 4.2;
+    var result = add(number1, number2);
+    console.log(result);
+}
