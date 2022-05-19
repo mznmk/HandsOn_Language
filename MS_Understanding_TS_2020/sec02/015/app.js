daac68cc945f9d429dafbf79f1a8687ad7b07868
mznmk <mika_develop@mznmk.com>
2022-05-19 16:30:36 +0900

    [MS TS] finish sec2-15

---
+++ MS_Understanding_TS_2020/sec02/015/app.js
@@ -0,0 +1,12 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'yota',
+//   age: 30
+// };
+var person = {
+    name: 'yota',
+    age: 30
+};
+console.log(person.name);
---
+++ MS_Understanding_TS_2020/sec02/015/app.ts
@@ -0,0 +1,13 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'yota',
+//   age: 30
+// };
+const person = {
+  name: 'yota',
+  age: 30
+}
+
+console.log(person.name);
