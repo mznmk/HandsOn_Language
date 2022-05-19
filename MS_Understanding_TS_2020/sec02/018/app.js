3f5664e32169562e94748e439749c154baac3d60
mznmk <mika_develop@mznmk.com>
2022-05-19 16:59:59 +0900

    [MS TS] finish sec2-18

---
+++ MS_Understanding_TS_2020/sec02/018/app.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'yota',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [];
+// person.role = [0, 'admin', 'user'];
